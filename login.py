edbae414d0afb540732f47ab199841944269629f
parsa <messiparsa83@gmail.com>
2026-02-14 14:55:00 +0330

    i change the login quickly

--- login.py
+++ login.py
@@ -1,6 +1,10 @@
-print("hello im login to github..... so lets go whats going on in future!")
+print ("hello im login to github..... so lets go whats going on in future!")
 
 
 a = 14
 b = 15
 print (a + b)
+
+
+print (" a it is equal to fourteen")
+
